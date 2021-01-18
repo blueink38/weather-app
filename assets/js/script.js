562b6b5b8a956b07b385be0d5664fe1d3b5d1c7d
muncle <blueink38@yahoo.com>
2021-01-18 13:21:02 -0600

    api call for current city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,57 @@
-// api key:  9d8f959959fa8dbc8906a07dad26301a
+const apiKey = "9d8f959959fa8dbc8906a07dad26301a"
 
-var
+$(document).ready(function() {
+    var cities = [];
+    $("#weather-0").hide();
+    $("#future-forcast").hide();
+
+    // begin api call for current city
+    function displayWeather(city) {
+        var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&units=imperial&appid=" + apiKey;
+
+        $.ajax( {
+            url: apiURL,
+            method: "GET"
+        }).then(function(response) {
+            weatherIcon = response.weather[0].icon;
+            var date = $("<h4>").text(moment().format('l'));
+            var icon = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png");
+
+            $("#city").text(response.name);
+            $("#city").append(date);
+            $("#city").append(icon);
+            $("#temp-0").text(response.main.temp);
+            $("#humidity-0").text(response.main.humidity);
+            $("#wind-0").text(response.wind.speed);
+
+                var lat = response.coord.lat;
+                var lon = response.coord.lon;
+                var apiUvUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly&appid=" + apiKey;
+
+                $.ajax( {
+                    url: apiUvURL,
+                    method: "GET"
+                }).then(function(response) {
+            $("#uv-0").text(response.daily.uvi);
+                var uvIndex = response.value;
+
+                $("#uv-0").removeClass("good");
+                $("#uv-0").removeClass("moderate");
+                $("#uv-0").removeClass("bad");
+
+                if (uvIndex <= 2.9) {
+                    $("uv-0").addClass("good"); 
+                } else if (uvIndex >=3 && uvIndex <= 5) {
+                    $("#uv-0").addClass("moderate");
+                } else {
+                    $("#uv-0").addClass("bad");
+                };
+            $("#uv-0").text(response.value);
+                });
+                $("#weather-0").show();
+            });
+    };
+    // end api call for current city
+
+    // begin api call for future forcast
+});
