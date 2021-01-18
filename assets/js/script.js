86bc6b9926d04f41834f683a28816b34aeeff2f4
muncle <blueink38@yahoo.com>
2021-01-17 20:51:56 -0600

    updated html id's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,51 @@
-let cities = [];
+// api key:  9d8f959959fa8dbc8906a07dad26301a
 
+var cityFormEl = document.querySelector("#city-input");
+var cityInputEl = document.querySelector("#city-choice");
+var cityWeatherToday = document.querySelector("#weather-0")
+var cityWeatherFutureOne = document.querySelector("#weather-1");
+var cityWeatherFutureTwo = document.querySelector("#weather-2");
+var cityWeatherFutureThree = document.querySelector("#weather-3");
+var cityWeatherFutureFour = document.querySelector("#weather-4");
+var cityWeatherFutureFive = document.querySelector("#weather-5");
+
+
+$(document).ready(function() {
+// get city and time, save to Local Storage
+let cities = [];
 const getWeather = (ev) => {
     ev.preventDefault();
     let city = {
         id: Date(),
-        city: document.getElementById('city').value
+        city: document.getElementById('city-choice').value.trim()
     }
 
     cities.push(city);
     // document.querySelector('form').reset();
     console.log(city);
-// save cities and date to Local Storage
+    // save cities and date to Local Storage
     localStorage.setItem('MyCityList', JSON.stringify(cities) );
+
+    var getCityWeather = function(city) {
+        var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial" + "9d8f959959fa8dbc8906a07dad26301a";
+
+        fetch(apiURL)
+            .then(function(response) {
+                if (response.ok) {
+                    console.log(response);
+                    response.json().then(function(data) {
+                        console.log(data);
+                        displayWeather(data, city);
+                    });
+                } else {
+                    alert("Error: " + response.statusText);
+                }
+            })
+    }
 
 }
 
+
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('btn').addEventListener('click', getWeather);
-})
+    document.getElementById('btn').addEventListener('click', getWeather)});
+});
---
+++ assets/js/test.js
@@ -0,0 +1,2 @@
+fetch("api.openweathermap.org/data/2.5/find?q=London&appid=01a")
+.then(res => console.log(res))
